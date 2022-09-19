89acf4445285b0f96729ec2bdd615cc6e1a6e7b0
Sandakan VPN Trickster <103347080+SandakanVPNTrickster@users.noreply.github.com>
2022-09-20 00:18:55 +0800

    Add files via upload

--- menu-bot.sh
+++ menu-bot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 clear
 ipes=$(curl -sS ipv4.icanhazip.com)
-surat=$(curl -sS https://raw.githubusercontent.com/SandakanVPNTrickster/MULTIPORT-WSS/main/permission/ip | grep -w $ipes | awk '{print $4}')
+surat=$(curl -sS https://raw.githubusercontent.com/SandakanVPNTrickster/main/pernission/ip | grep -w $ipes | awk '{print $4}')
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 if [[ "$surat" = "true" ]]; then
   echo -ne
@@ -24,7 +24,7 @@ dircreate() {
 fun_botOnOff() {
   dircreate
   [[ ! -f /root/multi/bot.conf ]] && {
-    echo -e "Aixxy Bot Panel Installer
+    echo -e "SquidVPN Bot Panel Installer
         "
     [[ ! -f /root/ResBotAuth ]] && {
       echo -ne "Input your Bot TOKEN : "
