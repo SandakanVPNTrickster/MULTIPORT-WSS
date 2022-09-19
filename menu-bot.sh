1fb1b55f2e9e629ba3376957b3983db162d7b393
Sandakan VPN Trickster <103347080+SandakanVPNTrickster@users.noreply.github.com>
2022-09-20 07:59:52 +0800

    Update menu-bot.sh

--- menu-bot.sh
+++ menu-bot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 clear
 ipes=$(curl -sS ipv4.icanhazip.com)
-surat=$(curl -sS https://raw.githubusercontent.com/SandakanVPNTrickster/permission/main/ip | grep -w $ipes | awk '{print $4}')
+surat=$(curl -sS https://raw.githubusercontent.com/SandakanVPNTrickster/permission/main/bot | grep -w $ipes | awk '{print $4}')
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 if [[ "$surat" = "true" ]]; then
   echo -ne
