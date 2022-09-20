2f80d1cefb0d038a8cc309aff7a5db681141556d
Sandakan VPN Trickster <103347080+SandakanVPNTrickster@users.noreply.github.com>
2022-09-20 12:30:16 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -23,6 +23,7 @@ wget -q -O /usr/bin/menu-dns "https://raw.githubusercontent.com/SandakanVPNTrick
 wget -q -O /usr/bin/info "https://raw.githubusercontent.com/SandakanVPNTrickster/MULTIPORT-WSS/main/info.sh" && chmod +x /usr/bin/info
 wget -q -O /usr/bin/mspeed "https://raw.githubusercontent.com/SandakanVPNTrickster/MULTIPORT-WSS/main/menu-speedtest.sh" && chmod +x /usr/bin/mspeed
 wget -q -O /usr/bin/mbandwith "https://raw.githubusercontent.com/SandakanVPNTrickster/MULTIPORT-WSS/main/menu-bandwith.sh" && chmod +x /usr/bin/mbandwith
+wget -q -O /usr/bin/sshwss "https://raw.githubusercontent.com/SandakanVPNTrickster/MULTIPORT-WSS/main/sshwss.sh" && chmod +x /usr/bin/sshwss
 wget -q -O /usr/bin/update "https://raw.githubusercontent.com/SandakanVPNTrickster/MULTIPORT-WSS/main/update.sh" && chmod +x /usr/bin/update
 echo -e " [INFO] Update Successfully"
 sleep 2
